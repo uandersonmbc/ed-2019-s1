ad7d2f9c163b1108456b130dff05a00bd230eeb7
Uanderson Nunes <uandersonmbc@hotmail.com>
2019-03-27 23:00:07 -0300

    Criado o embua

--- 04-Fractais/embua.c
+++ 04-Fractais/embua.c
@@ -6,17 +6,13 @@ void embua(int x, int y, float ang, int tamanho){
     int xf = x + tamanho * xm_cos(ang);
     int yf = y - tamanho * xm_sin(ang);
     xs_color((XColor){xm_rand(0, 255), xm_rand(0, 255), xm_rand(0, 255)});
-    xd_thick_line(x, y, xf, yf, 10);
+    xd_thick_line(x, y, xf, yf, 3);
     x_step("embua");
     embua(xf, yf, ang - 90, tamanho - 10);
-    x_step("embua");
-    xs_color(BLACK);
-    xd_thick_line(x, y, xf, yf, 10);
-    
 }
 
 int main(){
-    int largura = 600, altura = 600;
+    int largura = 1000, altura = 1000;
     x_open(largura, altura); 
     x_clear(BLACK);
     float ang = 90;
