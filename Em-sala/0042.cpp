9fc7bf57bd075d47e31ad1e6414a511dae65d284
Uanderson Nunes <uandersonmbc@hotmail.com>
2019-04-29 15:11:35 -0300

    Subindo feito em sala

---
+++ Em-sala/0042.cpp
@@ -0,0 +1,14 @@
+#include <map> //tree
+#include <set> //tree
+#include <unordered_map>
+#include <unordered_set> //hash
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main(){
+    vector<int> teste;
+    teste.push_back(5);
+    cout << teste[0] << endl;
+}
